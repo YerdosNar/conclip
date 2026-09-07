b26a614051f01a9a7a02fde39d163029aaad1e7d
YerdosNar <besway2000@gmail.com>
2026-09-07 16:21:17 +0900

    Added clipboard sending once

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+
+#define KB              1024
+#define PORT            8080
+#define CLIP_MAX        (64 * KB)
 
 typedef uint8_t         u8;
 typedef uint16_t        u16;
@@ -22,23 +27,46 @@ static void die(char *msg, i8 code)
         exit(code);
 }
 
+static char *clipboard_get(void)
+{
+        FILE *fp = popen("wl-paste --no-newline", "r");
+        if (!fp) die("popen", 1);
+
+        char *buffer = malloc(KB);
+        ssize_t n = fread(buffer, 1, KB, fp);
+
+        pclose(fp);
+        if (n > 0) {
+                buffer[n] = '\0';
+                return buffer;
+        }
+        return NULL;
+}
+
 int main()
 {
         i32 sfd = socket(AF_INET, SOCK_STREAM, 0);
         if (sfd == -1) die("socket", 1);
 
-        u16 port = 8080;
         struct sockaddr_in sa = {0};
         socklen_t sa_len = sizeof(sa);
 
         sa.sin_family = AF_INET;
         sa.sin_addr.s_addr = INADDR_ANY;
-        sa.sin_port = htons(port);
+        sa.sin_port = htons(PORT);
 
+        int opt = 1;
+        if (setsockopt(sfd, 
+                      SOL_SOCKET, 
+                      SO_REUSEADDR|SO_REUSEPORT, 
+                      &opt, sizeof(opt)) < 0) 
+        {
+                die("setsockopt", 1);
+        }
         if (bind(sfd, (struct sockaddr*)&sa, sa_len) < 0) die("bind", 1);
         if (listen(sfd, 5) < 0) die("listen", 1);
 
-        printf("Server listening: %d\n", port);
+        printf("Server listening: %d\n", PORT);
 
         struct sockaddr_in ca = {0};
         socklen_t ca_len = sizeof(ca);
@@ -65,6 +93,12 @@ int main()
                 buf[n] = '\0';
                 printf("[+] Received: %s\n", buf);
         }
+
+        char *clip = clipboard_get();
+        if (!clip) die("Empty clipboard", 1);
+        printf("CLIP: %s\n", clip);
+        n = send(new_con, clip, strlen(clip), 0);
+        if (n > 0) printf("[+] Sent: %s\n", clip);
 
         close(sfd);
         close(new_con);

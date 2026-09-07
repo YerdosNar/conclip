ef3dcf4d01e4cb7931e0b9d3c6acb895d28f23c6
YerdosNar <besway2000@gmail.com>
2026-09-07 12:35:25 +0900

    Added 'run' script to compile and cleaned up 'main.c' file

--- main.c
+++ main.c
@@ -1,126 +1,73 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <netinet/in.h>
 #include <stdint.h>
-#include <string.h>
-#include <unistd.h>
-#include <ifaddrs.h>
-#include <arpa/inet.h>
-
-static void die(char *msg, int status_code)
-{
-        perror(msg);
-        exit(status_code);
-}
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <ifaddrs.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
-#include <net/if.h>
+#include <unistd.h>
 
-int get_ip(char *buf, size_t buflen)
+typedef uint8_t         u8;
+typedef uint16_t        u16;
+typedef uint32_t        u32;
+typedef uint64_t        u64;
+
+typedef int8_t          i8;
+typedef int16_t         i16;
+typedef int32_t         i32;
+typedef int64_t         i64;
+
+static void die(char *msg, i8 code)
 {
-        struct ifaddrs *ifaddr;
-        struct ifaddrs *ifa;
-
-        if (getifaddrs(&ifaddr) == -1)
-                return -1;
-
-        for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
-
-                if (!ifa->ifa_addr)
-                        continue;
-
-                if (ifa->ifa_addr->sa_family != AF_INET)
-                        continue;
-
-                if (ifa->ifa_flags & IFF_LOOPBACK)
-                        continue;
-
-                if (!(ifa->ifa_flags & IFF_UP))
-                        continue;
-
-                struct sockaddr_in *addr =
-                        (struct sockaddr_in *)ifa->ifa_addr;
-
-                if (inet_ntop(AF_INET,
-                              &addr->sin_addr,
-                              buf,
-                              buflen) != NULL)
-                {
-                        freeifaddrs(ifaddr);
-                        return 0;
-                }
-        }
-
-        freeifaddrs(ifaddr);
-        return -1;
+        fprintf(stderr, "ERROR: %s failed\n", msg);
+        exit(code);
 }
+
 int main()
 {
-        uint16_t port = 8080;
-        struct sockaddr_in addr = {0};
-        int opt = 1;
-        socklen_t addrlen = sizeof(addr);
+        i32 sfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sfd == -1) die("socket", 1);
 
-        int s_fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (s_fd == -1) die("socket failed", 1);
+        u16 port = 8080;
+        struct sockaddr_in sa = {0};
+        socklen_t sa_len = sizeof(sa);
 
-        if (setsockopt(s_fd, 
-                                SOL_SOCKET, 
-                                SO_REUSEADDR|SO_REUSEPORT, 
-                                &opt, 
-                                sizeof(opt)))
-        {
-                die("setsockopt failed", 1);
-        }
+        sa.sin_family = AF_INET;
+        sa.sin_addr.s_addr = INADDR_ANY;
+        sa.sin_port = htons(port);
 
-        addr.sin_family = AF_INET;
-        addr.sin_addr.s_addr = INADDR_ANY;
-        addr.sin_port = htons(port);
+        if (bind(sfd, (struct sockaddr*)&sa, sa_len) < 0) die("bind", 1);
+        if (listen(sfd, 5) < 0) die("listen", 1);
 
-        if (bind(s_fd, (struct sockaddr*)&addr, addrlen) < 0) die("bind failed", 1);
-        if (listen(s_fd, 5) < 0) die("listen failed", 1);
+        printf("Server listening: %d\n", port);
 
-        char ip[INET_ADDRSTRLEN];
-        if (get_ip(ip, sizeof(ip)) == -1) {
-                fprintf(stderr, "ERROR: No IPv4 interface found\n");
-                return 1;
-        }
-
-
-        printf("Server is on %s:%d\n", ip, port);
-
-        struct sockaddr_in cl_addr;
-        socklen_t cl_len = sizeof(cl_addr);
-        int new_con;
-        new_con = accept(s_fd, 
-                        (struct sockaddr*)&addr, 
-                        &cl_len
-                        );
+        struct sockaddr_in ca = {0};
+        socklen_t ca_len = sizeof(ca);
+        i32 new_con = accept(sfd,
+                        (struct sockaddr*)&ca,
+                        &ca_len
+        );
 
         char cl_ip[INET_ADDRSTRLEN];
-        inet_ntop(
-                        AF_INET,
-                        &cl_addr.sin_addr,
-                        cl_ip,
-                        sizeof(cl_ip)
-                 );
+        inet_ntop(AF_INET,
+                  &ca.sin_addr,
+                  cl_ip,
+                  sizeof(cl_ip)
+        );
 
         printf("[+] Client from %s:%d\n",
                         cl_ip,
-                        ntohs(cl_addr.sin_port)
-              );
+                        ntohs(ca.sin_port)
+        );
+
         char buf[1024];
         ssize_t n = recv(new_con, buf, sizeof(buf) - 1, 0);
         if (n > 0) {
                 buf[n] = '\0';
-                printf("Received: %s\n", buf);
+                printf("[+] Received: %s\n", buf);
         }
 
+        close(sfd);
         close(new_con);
-        close(s_fd);
 
         return 0;
 }
